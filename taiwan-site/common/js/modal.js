0c322a2d5a9513de4eac874eb065c7828900b01d
hallow-wa-ker <torikawadaisukitv@gmail.com>
2024-02-09 14:06:59 +0900

    mo-daru完成

--- taiwan-site/common/js/modal.js
+++ taiwan-site/common/js/modal.js
@@ -4,8 +4,13 @@
 'use strict'
 
 const modal = document.querySelector('.modal')
-const openbutton = document.getElementById('modal-openbutton')
+const modal2 = document.querySelector('.modal2')
+const modal3 = document.querySelector('.modal3')
+const modal4 = document.querySelector('.modal4')
 const closebutton = document.querySelector('.modalclose')
+const closebutton2 = document.querySelector('.modalclose2')
+const closebutton3 = document.querySelector('.modalclose3')
+const closebutton4 = document.querySelector('.modalclose4')
 
 function Open() {
   modal.classList.add('Opening')
@@ -16,5 +21,29 @@ function Close() {
 }
 closebutton.addEventListener('click', Close);
 
+function Open2() {
+  modal2.classList.add('Opening2')
+}
 
+function Close2() {
+  modal2.classList.remove('Opening2');
+}
+closebutton2.addEventListener('click', Close2);
 
+function Open3() {
+  modal3.classList.add('Opening3')
+}
+
+function Close3() {
+  modal3.classList.remove('Opening3');
+}
+closebutton3.addEventListener('click', Close3);
+
+function Open4() {
+  modal4.classList.add('Opening4')
+}
+
+function Close4() {
+  modal4.classList.remove('Opening4');
+}
+closebutton4.addEventListener('click', Close4);
