22f78519154d56fb3c8b612c123b805c2ceefde7
akko-maker <wildorangeandsunbeams@gmail.com>
2024-02-15 14:09:08 +0900

    pagetop.js更新

--- taiwan-site/common/js/pagetop.js
+++ taiwan-site/common/js/pagetop.js
@@ -3,3 +3,21 @@
 
 'use strict'
 
+jQuery(function () {
+
+  var footer = $('.kasou-main').innerHeight(); // footerの高さを取得
+
+  window.onscroll = function () {
+    var point = window.pageYOffset; // 現在のスクロール地点 
+    var docHeight = $(document).height(); // ドキュメントの高さ
+    var dispHeight = $(window).height(); // 表示領域の高さ
+
+    if (point > docHeight - dispHeight - footer) { // スクロール地点>ドキュメントの高さ-表示領域-footerの高さ
+      $('.pagetop').addClass('is-hidden'); //footerより下にスクロールしたらis-hiddenを追加
+    } else {
+      $('.pagetop').removeClass('is-hidden'); //footerより上にスクロールしたらis-hiddenを削除
+    }
+  };
+});
+
+
